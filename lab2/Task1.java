6bbe75ea8177f40f9564793813094b886d347f53
Chang Bok Lee <cb3lee@uwaterloo.ca>
2018-06-16 15:56:47 -0400

    task1 hadoop

--- lab2/Task1.java
+++ lab2/Task1.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.LinkedList;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.ArrayPrimitiveWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,6 +15,51 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Task1 {
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
+    private Text title = new Text();
+    private Text ratings = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] tokens = value.toString().split(",");
+      LinkedList<String> idWithMaxRatings = new LinkedList<>();
+      title.set(tokens[0]);
+      int currentMax = 1;
+      for (int i = 1; i < tokens.length; i++) {
+        int current = 0;
+        try {
+          current = Integer.parseInt(tokens[i]);
+        } catch (Exception e) { }
+        if (current > currentMax) {
+          idWithMaxRatings.clear();
+          idWithMaxRatings.add(String.valueOf(i));
+          currentMax = current;
+        } else if (current == currentMax) {
+          idWithMaxRatings.add(String.valueOf(i));
+        }
+      }
+      ratings.set(String.join(",", idWithMaxRatings));
+      context.write(title, ratings);
+    }
+  }
+
+
+
   public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.set("mapred.textoutputformat.separator", ",");
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: task1 <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "task 1");
+    job.setJar("Task1.jar");
+    job.setMapperClass(TokenizerMapper.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    job.setNumReduceTasks(0);
+    TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
---
+++ lab2/buildrun_task_hadoop_ecelinux.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+unset JAVA_TOOL_OPTIONS
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0
+export HADOOP_HOME=/opt/hadoop-2.7.2
+export CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath`
+
+echo --- Deleting
+rm Task$1.jar
+rm Task$1*.class
+
+echo --- Compiling
+$JAVA_HOME/bin/javac Task$1.java
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+echo --- Jarring
+$JAVA_HOME/bin/jar -cf Task$1.jar Task$1*.class
+
+echo --- Running
+INPUT=sample_input/smalldata.txt
+OUTPUT=output_hadoop_task$1
+
+rm -fr $OUTPUT
+$HADOOP_HOME/bin/hadoop jar Task$1.jar Task$1 $INPUT $OUTPUT
+
+cat $OUTPUT/*
