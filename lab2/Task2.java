45a9c3684fe5323b11a52022463170c96fa6e783
Chang Bok Lee <cb3lee@uwaterloo.ca>
2018-06-19 18:55:01 -0400

    task2 hadoop

--- lab2/Task2.java
+++ lab2/Task2.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,6 +14,48 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Task2 {
+  public static class Task2Mapper extends Mapper<Object, Text, Text, IntWritable> {
+    private Text dummy = new Text("count");
+    private IntWritable one = new IntWritable(1);
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] tokens = value.toString().split(",");
+      for (int i = 1; i < tokens.length; i++) {
+        String token = tokens[i];
+        if (token.length() > 0) {
+          context.write(dummy, one);
+        }
+      }
+    }
+  }
+
+  public static class IntSumReducer extends Reducer<Text,IntWritable,NullWritable,IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable v : values) {
+        sum += v.get();
+      }
+      context.write(NullWritable.get(), new IntWritable(sum));
+    }
+  }
+
   public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.set("mapred.textoutputformat.separator", ",");
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: task2 <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "task 2");
+    job.setJar("Task2.jar");
+    job.setMapperClass(Task2Mapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setNumReduceTasks(1);
+    TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
