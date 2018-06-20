7bb3d7b673a5eb104ff2b13f134387efb5282371
Chang Bok Lee <cb3lee@uwaterloo.ca>
2018-06-19 20:17:36 -0400

    task2 scala

--- lab2/Task2.scala
+++ lab2/Task2.scala
@@ -8,8 +8,18 @@ object Task2 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
-    
+    val output = textFile.map(line => {
+      val key = ""
+      val splited = line.split(",")
+      val count = splited.drop(1).foldLeft(0)((x, y) => {
+        if (y.length > 0) {
+          x + 1
+        } else {
+          x
+        }
+      })
+      (key, count)
+    }).reduceByKey(_ + _).map(x => x._2)
     output.saveAsTextFile(args(1))
   }
 }
