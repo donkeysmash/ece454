8954368d4407fbe4aa3780c8dd3f2428b85e8843
sang4441 <skim4441@gmail.com>
2018-07-07 21:21:48 -0400

    updatE

--- lab3/KeyValueHandler.java
+++ lab3/KeyValueHandler.java
@@ -19,26 +19,76 @@ public class KeyValueHandler implements KeyValueService.Iface {
     private String zkNode;
     private String host;
     private int port;
+    private KeyValueService.Client clientBackup;
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public KeyValueHandler(String host, int port, CuratorFramework curClient, String zkNode) {
-	this.host = host;
-	this.port = port;
-	this.curClient = curClient;
-	this.zkNode = zkNode;
-	myMap = new ConcurrentHashMap<String, String>();	
+    	this.host = host;
+    	this.port = port;
+    	this.curClient = curClient;
+    	this.zkNode = zkNode;
+    	this.myMap = new ConcurrentHashMap<String, String>();
+
+        //decide who's primary here	
     }
 
     public String get(String key) throws org.apache.thrift.TException
     {	
-	String ret = myMap.get(key);
-	if (ret == null)
-	    return "";
-	else
-	    return ret;
+    	String ret = myMap.get(key);
+    	if (ret == null)
+    	    return "";
+    	else
+    	    return ret;
     }
 
     public void put(String key, String value) throws org.apache.thrift.TException
     {
-	myMap.put(key, value);
+	    myMap.put(key, value);
+        List<String> children = curClient.getChildren().forPath(zkNode);
+        if (children.size == 1) {
+            clientBackup = null
+        } else if (children.size() > 1 && clientBackup != null) {
+            clientBackup.putBackup(key, value);
+        } else {
+            initBackUpClient();
+            clientBackup.putBackup(key, value);
+        }
     }
+
+    public void putBackup(String key, String value) throws org.apache.thrift.TException
+    {
+        lock.writeLock().lock();
+        try{
+            myMap.put(key, value);
+        } catch (Exception e) {
+            lock.writeLock().unlock();
+        } finally{
+            lock.writeLock().unlock();
+        }
+    }
+
+    public void initBackUpClient() {
+      
+        List<String> children = curClient.getChildren().usingWatcher(this).forPath(zkNode);
+        if (children.size() > 1) {
+            Collections.sort(children);
+            byte[] payloadPrimary = curClient.getData().forPath(zkNode + "/" + children.get(0));    
+            String ipAddressPrimary = new String(payloadPrimary);
+            String[] ipAddressPrimaryArray = ipAddressPrimary.split(":");
+            TSocket sock = new TSocket(ipAddressPrimaryArray[0], Integer.parseInt(ipAddressPrimaryArray[1]));
+            TTransport transport = new TFramedTransport(sock);
+            transport.open();
+            TProtocol protocol = new TBinaryProtocol(transport);
+            clientBackup = KeyValueService.Client(protocol);
+        } 
+    }
+
+    public void copyData(data) throws org.apache.thrift.TException{
+        myMap = data;
+    }
+
+    public Map<String, String> getData() throws org.apache.thrift.TException{
+        return myMap;
+    }
+
 }
--- lab3/StorageNode.java
+++ lab3/StorageNode.java
@@ -19,46 +19,91 @@ public class StorageNode {
     static Logger log;
 
     public static void main(String [] args) throws Exception {
-	BasicConfigurator.configure();
-	log = Logger.getLogger(StorageNode.class.getName());
+		BasicConfigurator.configure();
+		log = Logger.getLogger(StorageNode.class.getName());
 
-	if (args.length != 4) {
-	    System.err.println("Usage: java StorageNode host port zkconnectstring zknode");
-	    System.exit(-1);
-	}
-
-	CuratorFramework curClient =
-	    CuratorFrameworkFactory.builder()
-	    .connectString(args[2])
-	    .retryPolicy(new RetryNTimes(10, 1000))
-	    .connectionTimeoutMs(1000)
-	    .sessionTimeoutMs(10000)
-	    .build();
-
-	curClient.start();
-	Runtime.getRuntime().addShutdownHook(new Thread() {
-		public void run() {
-		    curClient.close();
+		if (args.length != 4) {
+		    System.err.println("Usage: java StorageNode host port zkconnectstring zknode");
+		    System.exit(-1);
 		}
+
+		CuratorFramework curClient =
+		    CuratorFrameworkFactory.builder()
+		    .connectString(args[2])
+		    .retryPolicy(new RetryNTimes(10, 1000))
+		    .connectionTimeoutMs(1000)
+		    .sessionTimeoutMs(10000)
+		    .build();
+
+		curClient.start();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+			    curClient.close();
+			}
 	    });
 
-	KeyValueService.Processor<KeyValueService.Iface> processor = new KeyValueService.Processor<>(new KeyValueHandler(args[0], Integer.parseInt(args[1]), curClient, args[3]));
-	TServerSocket socket = new TServerSocket(Integer.parseInt(args[1]));
-	TThreadPoolServer.Args sargs = new TThreadPoolServer.Args(socket);
-	sargs.protocolFactory(new TBinaryProtocol.Factory());
-	sargs.transportFactory(new TFramedTransport.Factory());
-	sargs.processorFactory(new TProcessorFactory(processor));
-	sargs.maxWorkerThreads(64);
-	TServer server = new TThreadPoolServer(sargs);
-	log.info("Launching server");
+		KeyValueService.Processor<KeyValueService.Iface> processor = new KeyValueService.Processor<>(new KeyValueHandler(args[0], Integer.parseInt(args[1]), curClient, args[3]));
+		TServerSocket socket = new TServerSocket(Integer.parseInt(args[1]));
+		TThreadPoolServer.Args sargs = new TThreadPoolServer.Args(socket);
+		sargs.protocolFactory(new TBinaryProtocol.Factory());
+		sargs.transportFactory(new TFramedTransport.Factory());
+		sargs.processorFactory(new TProcessorFactory(processor));
+		sargs.maxWorkerThreads(64);
+		TServer server = new TThreadPoolServer(sargs);
+		log.info("Launching server");
 
-	new Thread(new Runnable() {
-		public void run() {
-		    server.serve();
-		}
+		new Thread(new Runnable() {
+			public void run() {
+			    server.serve();
+			}
 	    }).start();
 
-	// TODO: create an ephemeral node in ZooKeeper
-	// curClient.create(...)
+		String ipAddress = args[0] + ":" + args[1];
+		byte[] payload = ipAddress.getBytes();
+		String zkNode = args[3];
+		curClient.create().withMode(CreateMode.EPHEMERAL).forPath(zkNode, payload);
+
+		List<String> children = curClient.getChildren().forPath(zkNode);
+		Collections.sort(children);
+		byte[] payloadPrimary = curClient.getData().forPath(zkNode + "/" + children.get(0));	
+		String ipAddressPrimary = new String(payloadPrimary);
+
+		if (ipAddressPrimary.equals(ipAddress)) {
+			//this is primary
+			//implement concurrency control
+		} else {
+			//this is back up
+		}
+
+		if (children.size() > 1) {
+			List<String> children = curClient.getChildren().forPath(args[3]);
+			Collections.sort(children);
+			for (String child : children) {
+				String ipAddressChild = new String(curClient.getData().forPath(zkNode + "/" + child));
+				if (!ipAddressChild.equals(ipAddress)) {
+			    	
+
+			    	String[] ipAddressFromArr = ipAddressChild.split(":");
+					TSocket sockFrom = new TSocket(ipAddressFromArr[0], Integer.parseInt(ipAddressFromArr[1]));
+			        TTransport transport = new TFramedTransport(sockFrom);
+			        transport.open();
+			        TProtocol protocol = new TBinaryProtocol(transport);
+					KeyValueService.Client clientBackup = new KeyValueService.Client(protocol);
+					Map<String, String> data = clientBackup.copyData();
+					transport.close();
+
+					String[] ipAddressToArr = ipAddress.split(":");
+					TSocket sockTo = new TSocket(ipAddressToArr[0], Integer.parseInt(ipAddressToArr[1]));
+			        TTransport transport = new TFramedTransport(sockTo);
+			        transport.open();
+			        TProtocol protocol = new TBinaryProtocol(transport);
+					KeyValueService.Client clientBackup = new KeyValueService.Client(protocol);
+					clientBackup.copyData(data);
+
+					break;
+				}
+			}
+		
+		} //needs replication
     }
 }
