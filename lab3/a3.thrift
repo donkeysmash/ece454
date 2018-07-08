2ad0ddcd0eb48a02dde7f362c08d97490e12efb0
sang4441 <skim4441@gmail.com>
2018-07-08 01:18:13 -0400

    update thrist

--- lab3/a3.thrift
+++ lab3/a3.thrift
@@ -1,4 +1,8 @@
 service KeyValueService {
   string get(1: string key);
   void put(1: string key, 2: string value);
+  void putBackup(1: string key, 2: string value);
+  void initBackUpClient(); 
+  void copyData(1: Map<String, String> data);
+  Map<String, String> getData();  
 }
